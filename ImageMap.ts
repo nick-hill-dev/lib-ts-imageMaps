685793c4acba6d2cf41e190d52a34fece005fdc1
hg <none@none>
2018-08-17 17:27:56 +0100

    Add a NineImageMap class for rendering 9-patches.

--- ImageMap.ts
+++ ImageMap.ts
@@ -8,16 +8,36 @@
 
         public readonly count: number = 0;
 
-        public constructor(public readonly image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement, public readonly tileWidth: number, public readonly tileHeight: number) {
-            this.tilesWide = Math.ceil(image.width / tileWidth);
-            this.tilesHigh = Math.ceil(image.height / tileHeight);
+        public constructor(
+            public readonly image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement,
+            public readonly tileWidth: number = 0,
+            public readonly tileHeight: number = 0
+        ) {
+            if (tileWidth == 0) {
+                this.tileWidth = image.width;
+            }
+            if (tileHeight == 0) {
+                this.tileHeight = image.height;
+            }
+            this.tilesWide = Math.ceil(image.width / this.tileWidth);
+            this.tilesHigh = Math.ceil(image.height / this.tileHeight);
             this.count = this.tilesWide * this.tilesHigh;
         }
 
         public render(context: CanvasRenderingContext2D, x: number, y: number, imageIndex: number) {
             var imageIndexX = imageIndex % this.tilesWide;
             var imageIndexY = Math.floor(imageIndex / this.tilesWide);
-            context.drawImage(this.image, imageIndexX * this.tileWidth, imageIndexY * this.tileHeight, this.tileWidth, this.tileHeight, x, y, this.tileWidth, this.tileHeight);
+            context.drawImage(
+                this.image,
+                imageIndexX * this.tileWidth,
+                imageIndexY * this.tileHeight,
+                this.tileWidth,
+                this.tileHeight,
+                x,
+                y,
+                this.tileWidth,
+                this.tileHeight
+            );
         }
 
     }
