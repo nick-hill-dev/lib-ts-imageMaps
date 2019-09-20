96e54debdbad9ba8d7d3c59f5920a392b7f085f4
nick <none@none>
2019-09-20 12:30:55 +0100

    Add font image map.

---
+++ src/FontImageMap.ts
@@ -0,0 +1,25 @@
+﻿namespace ImageMaps {
+
+    export class FontImageMap {
+
+        private imageMap: ImageMap;
+
+        public constructor(
+            image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement,
+            glyphWidth: number = 0,
+            glyphHeight: number = 0
+        ) {
+            this.imageMap = new ImageMap(image, glyphWidth, glyphHeight);
+        }
+
+        public render(context: CanvasRenderingContext2D, x: number, y: number, text: string) {
+            for (let i = 0; i < text.length; i++) {
+                let code = text.charCodeAt(i);
+                this.imageMap.render(context, x, y, code);
+                x += this.imageMap.tileWidth;
+            }
+        }
+
+    }
+
+}
